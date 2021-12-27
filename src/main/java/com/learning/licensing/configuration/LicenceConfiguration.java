6c02e95f1f6b86c418d44133a1d2fd6e2bc90385
vitned <vitalii.nedbaliuk@gmail.com>
2021-12-27 16:24:48 +0100

    LP-72 remove Eureka Server as service discovery already exits in K8S

--- src/main/java/com/learning/licensing/configuration/LicenceConfiguration.java
+++ src/main/java/com/learning/licensing/configuration/LicenceConfiguration.java
@@ -1,13 +1,11 @@
 package com.learning.licensing.configuration;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class LicenceConfiguration {
-    @LoadBalanced
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
